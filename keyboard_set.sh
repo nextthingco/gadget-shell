cd730a832501a181f8c3ab6a4344ce302524005d
computermouth <computermouth@crunchbangplusplus.org>
2017-04-01 00:51:35 -0700

    set fake monitor

--- keyboard_set.sh
+++ keyboard_set.sh
@@ -18,3 +18,28 @@ sudo debconf-set-selections <<< "keyboard-configuration keyboard-configuration/u
 sudo debconf-set-selections <<< "keyboard-configuration keyboard-configuration/variantcode string "
 sudo debconf-set-selections <<< "keyboard-configuration keyboard-configuration/variant select German"
 sudo debconf-set-selections <<< "keyboard-configuration keyboard-configuration/xkb-keymap select "
+
+
+cat << EOF > | sudo tee /usr/share/X11/xorg.conf.d/xorg.conf
+Section "Device"
+    Identifier  "Configured Video Device"
+    Driver      "dummy"
+EndSection
+
+Section "Monitor"
+    Identifier  "Configured Monitor"
+    HorizSync 31.5-48.5
+    VertRefresh 50-70
+EndSection
+
+Section "Screen"
+    Identifier  "Default Screen"
+    Monitor     "Configured Monitor"
+    Device      "Configured Video Device"
+    DefaultDepth 24
+    SubSection "Display"
+    Depth 24
+    Modes "1024x800"
+    EndSubSection
+EndSection
+EOF 
